d221daed1c5848920a59df03b4f10d4b94235dc0
Aleksey Yuriev <alex11-90@yandex.by>
2023-07-11 00:16:56 +0300

    feat: validation function added

--- scripts/index.js
+++ scripts/index.js
@@ -64,7 +64,91 @@ initialCards.forEach((item) => {
 const editButtonElement = document.querySelector('.profile__edit-button');
 const popupProfileCloseButton = document.querySelector('.popup__close-button_type_profile');
 const popupEditElement = document.querySelector('.popup_type_edit');
-const formEditElement = document.querySelector('.popup__form_type_edit');
+const formEditElement = document.querySelector('.popup__form_type_edit')
+
+
+
+
+
+
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error');
+  errorElement.textContent = '';
+};
+
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement);
+
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault;
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+
+function hasInvalidInput (inputList) {
+  return inputList.some((inputElement) => {
+
+    return !inputElement.validity.valid;
+  })
+};
+
+function toggleButtonState (inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-button_disabled');
+    buttonElement.setAttribute('disabled', 'disabled');
+  } else {
+    buttonElement.classList.remove('popup__save-button_disabled');
+    buttonElement.removeAttribute('disabled');
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
 
 const nameInput = document.querySelector('.popup__input_type_name');
 const descriptionInput = document.querySelector('.popup__input_type_description');
